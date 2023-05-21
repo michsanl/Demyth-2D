ba027009982ae097d1178a3c67d439ca4170da7b
michsanl <desk.ichsan101@gmail.com>
2023-05-22 04:14:05 +0700

    Created Audio Manager & Sri audio resource SO

---
+++ Assets/@Productions/Scripts/Scriptable Objects/AudioClipSriSO.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class AudioClipSriSO : ScriptableObject
+{
+    public AudioClip[] Damage;
+    public AudioClip HorizontalSlash;
+    public AudioClip VerticalSlash;
+    public AudioClip SpinClaw;
+    public AudioClip Fireball;
+    public AudioClip NailAOE;
+    public AudioClip NailSummon;
+    public AudioClip GroundNail;
+    public AudioClip FlyingNailSummon;
+    public AudioClip FlyingNailShot;
+    public AudioClip FlyingNailImpact;
+}
