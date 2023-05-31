7f77b4831a82ee4e0aeeea3884d09c6ba09770cd
michsanl <desk.ichsan101@gmail.com>
2023-05-31 08:56:01 +0700

    Fix ground nail ability collider

--- Assets/@Productions/Scripts/Unit/Boss Sri/Ability/SummonedNail.cs
+++ Assets/@Productions/Scripts/Unit/Boss Sri/Ability/SummonedNail.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 public class SummonedNail : MonoBehaviour
 {
-    [SerializeField] private GameObject nailModel;
+    [SerializeField] private GameObject model;
     [SerializeField] private GameObject anticipationModel;
+    [SerializeField] private GameObject colliderGO;
     [SerializeField] private float destroyTimer;
     [SerializeField] private float summonDelayTimer;
 
-    private void Start() 
+    private void Awake() 
     {
-        StartCoroutine(DestroyMe());
+        StartCoroutine(SpawnNailModel());
+        StartCoroutine(SpawnCollider());
+        StartCoroutine(DestroySelf());
     }
 
-    public IEnumerator DestroyMe()
+    private IEnumerator SpawnNailModel()
     {
         yield return Helper.GetWaitForSeconds(summonDelayTimer);
+        model.SetActive(true);
         anticipationModel.SetActive(false);
-        nailModel.SetActive(true);
-        
+    }
+
+    private IEnumerator SpawnCollider()
+    {
+        yield return Helper.GetWaitForSeconds(summonDelayTimer + 0.1333f);
+        colliderGO.SetActive(true);
+    }
+
+    private IEnumerator DestroySelf()
+    {   
         yield return Helper.GetWaitForSeconds(destroyTimer);
         Destroy(gameObject);
     }
