4f919a7361a10080bb58cd3d3f1d97b064ab1585
michsanl <desk.ichsan101@gmail.com>
2023-07-04 04:51:37 +0700

    Create tuyul movement behavior at level 5

---
+++ Assets/@Productions/Scripts/Unit/NPC Tuyul/TuyulMoveTrigger.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TuyulMoveTrigger : MonoBehaviour
+{
+
+    public Direction direction;
+    public enum Direction { Up, Down, Right, Left }
+
+    [SerializeField] private TuyulMovement tuyulMovement;
+
+    private Vector3 moveDir;
+
+    private void Awake() 
+    {
+        switch (direction)
+        {
+            case Direction.Up:
+                moveDir = Vector3.up;
+                break;
+            case Direction.Down:
+                moveDir = Vector3.down;
+                break;
+            case Direction.Right:
+                moveDir = Vector3.right;
+                break;
+            case Direction.Left:
+                moveDir = Vector3.left;
+                break;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        tuyulMovement.StartCoroutine(tuyulMovement.Flee(moveDir));
+    }
+}
