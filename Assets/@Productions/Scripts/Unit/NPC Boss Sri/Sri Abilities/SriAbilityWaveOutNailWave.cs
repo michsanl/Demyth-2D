4040e48d2dd54257a5a10be29ad45d2dc0b9e763
michsanl <desk.ichsan101@gmail.com>
2023-07-10 21:20:54 +0700

    Add teleport to sri ability wave out nail

--- Assets/@Productions/Scripts/Unit/NPC Boss Sri/Sri Abilities/SriAbilityWaveOutNailWave.cs
+++ Assets/@Productions/Scripts/Unit/NPC Boss Sri/Sri Abilities/SriAbilityWaveOutNailWave.cs
@@ -14,23 +14,40 @@ public class SriAbilityWaveOutNailWave : SceneService
     [SerializeField] private Animator animator;
     [SerializeField] private GameObject waveOutNailWave;
     
-    protected int NAIL_WAVE = Animator.StringToHash("Intro");
+    private float teleportStartDuration = 0.3f;
+    private float teleportEndDuration = 0.4f;
+    private int TELEPORT_START = Animator.StringToHash("Teleport_Start");
+    private int TELEPORT_END = Animator.StringToHash("Teleport_End");
+    private int NAIL_WAVE = Animator.StringToHash("Intro");
 
     public IEnumerator PlayAbility()
     {
         var audioManager = Context.AudioManager;
 
+        yield return StartCoroutine(TeleportToMiddleArena());
+
         animator.Play(NAIL_WAVE);
-        // audioManager.PlayClipAtPoint(audioManager.SriAudioSource.NailSummon, transform.position);
+        // audioManager.PlayClipAtPoint(audioManager.SriAudioSource.NailAOE, transform.position);
         StartCoroutine(SpawnNail());
 
         yield return Helper.GetWaitForSeconds(animationDuration);
     }
 
+    public IEnumerator TeleportToMiddleArena()
+    {
+        animator.Play(TELEPORT_START);
+        yield return Helper.GetWaitForSeconds(teleportStartDuration);
+
+        transform.position = new Vector3(0, -1, 0);
+
+        animator.Play(TELEPORT_END);
+        yield return Helper.GetWaitForSeconds(teleportEndDuration);
+    }
+
     private IEnumerator SpawnNail()
     {
         yield return Helper.GetWaitForSeconds(nailSpawnDelay);
-        
+
         Instantiate(waveOutNailWave, new Vector3(0, -1, 0), Quaternion.identity);
     }
 }
