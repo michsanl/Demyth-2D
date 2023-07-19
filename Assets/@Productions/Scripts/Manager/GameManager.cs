ed21f627817dd336e24a0ffa2cfa7bc06a14108a
michsanl <desk.ichsan101@gmail.com>
2023-07-19 16:34:59 +0700

    Set disable player game input on conversation start

--- Assets/@Productions/Scripts/Manager/GameManager.cs
+++ Assets/@Productions/Scripts/Manager/GameManager.cs
@@ -2,10 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using CustomTools.Core;
-using UISystem;
-using DG.Tweening;
 
 public class GameManager : SceneService
 {
