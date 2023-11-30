3560b87e9b44b96140b6303103686fa5ec1d7e80
michsanl <desk.ichsan101@gmail.com>
2023-11-30 14:01:35 +0700

    Refactor player hit effect

--- Assets/@Productions/Scripts/Unit/Player/Player.cs
+++ Assets/@Productions/Scripts/Unit/Player/Player.cs
@@ -6,9 +6,16 @@ using Demyth.Gameplay;
 using echo17.Signaler.Core;
 using Core;
 using MoreMountains.Tools;
+using MoreMountains.Feedbacks;
 
 public class Player : MonoBehaviour, IBroadcaster
 {
+    public Action OnInvulnerableVisualStart;
+    public Action OnInvulnerableVisualEnd;
+    public Action<bool> OnSenterToggle;
+    public Vector2 LastMoveTargetPosition => _moveTargetPosition;
+    public Vector2 PlayerDir => _playerDir;
+
     [Title("Settings")]
     [SerializeField] private float actionDuration;
     [SerializeField] private float attackDuration;
@@ -16,41 +23,33 @@ public class Player : MonoBehaviour, IBroadcaster
     [SerializeField] private LayerMask moveBlockMask;
     
     [Title("Components")]
+    [SerializeField] private MMF_Player _hitFlashEffectMMFPlayer;
     [SerializeField] private Animator animator;
     [SerializeField] private Animator damagedAnimator;
     [SerializeField] private AudioClipAraSO araAudioSO;
     [SerializeField] private GameObject senterGameObject;
     [SerializeField] private GameObject hitEffect;
-    
-#region Public Fields
-    public Action OnInvulnerableVisualStart;
-    public Action OnInvulnerableVisualEnd;
-    public Action<bool> OnSenterToggle;
-    public Vector2 LastMoveTargetPosition => moveTargetPosition;
-    public Vector2 PlayerDir => playerDir;
 
-#endregion
-
-    private LookOrientation lookOrientation;
-    private HealthPotion healthPotion;
-    private Health health;
-    private Vector2 playerDir;
-    private Vector2 moveTargetPosition;
-    private bool isBusy;
-    private bool isKnocked;
-    private bool isTakeDamageOnCooldown;
-    private bool isSenterEnabled;
-    private bool isSenterUnlocked = true;
-    private bool isHealthPotionUnlocked = true;
+    private LookOrientation _lookOrientation;
+    private HealthPotion _healthPotion;
+    private Health _health;
+    private Vector2 _playerDir;
+    private Vector2 _moveTargetPosition;
+    private bool _isBusy;
+    private bool _isKnocked;
+    private bool _isTakeDamageOnCooldown;
+    private bool _isSenterEnabled;
+    private bool _isSenterUnlocked = true;
+    private bool _isHealthPotionUnlocked = true;
 
     private GameInputController _gameInputController;
     private GameInput _gameInput;
 
     private void Awake()
     {
-        lookOrientation = GetComponent<LookOrientation>();
-        healthPotion = GetComponent<HealthPotion>();
-        health = GetComponent<Health>();
+        _lookOrientation = GetComponent<LookOrientation>();
+        _healthPotion = GetComponent<HealthPotion>();
+        _health = GetComponent<Health>();
 
         _gameInputController = SceneServiceProvider.GetService<GameInputController>();
         _gameInput = _gameInputController.GameInput;
@@ -68,7 +67,7 @@ public class Player : MonoBehaviour, IBroadcaster
 
     private void OnEnable()
     {
-        moveTargetPosition = transform.position;
+        _moveTargetPosition = transform.position;
 
         _gameInput.OnSenterPerformed.AddListener(GameInput_OnSenterPerformed);
         _gameInput.OnHealthPotionPerformed.AddListener(GameInput_OnHealthPotionPerformed);
@@ -82,10 +81,10 @@ public class Player : MonoBehaviour, IBroadcaster
 
     public void ApplyDamageToPlayer(bool enableKnockBack, Vector2 knockbackTargetPosition)
     {
-        if (isTakeDamageOnCooldown)
+        if (_isTakeDamageOnCooldown)
             return;
         if (enableKnockBack)
-            isKnocked = true;
+            _isKnocked = true;
 
         StartCoroutine(ApplyDamageToPlayerCoroutine(enableKnockBack, knockbackTargetPosition));
     }
@@ -98,9 +97,9 @@ public class Player : MonoBehaviour, IBroadcaster
     
     private void HandlePlayerAction()
     {
-        if (isKnocked)
+        if (_isKnocked)
             return;
-        if (isBusy)
+        if (_isBusy)
             return;
 
         Vector2 inputVector = _gameInput.GetMovementVector();
@@ -110,10 +109,10 @@ public class Player : MonoBehaviour, IBroadcaster
         if (IsInputVectorDiagonal(inputVector))
             return;
 
-        playerDir = inputVector;
-        lookOrientation.SetFacingDirection(playerDir);
+        _playerDir = inputVector;
+        _lookOrientation.SetFacingDirection(_playerDir);
         
-        if (Helper.CheckTargetDirection(transform.position, playerDir, moveBlockMask, out Interactable interactable))
+        if (Helper.CheckTargetDirection(transform.position, _playerDir, moveBlockMask, out Interactable interactable))
         {
             if (interactable == null)
                 return;
@@ -128,77 +127,77 @@ public class Player : MonoBehaviour, IBroadcaster
 
     private IEnumerator HandleMovement()
     {
-        isBusy = true;
+        _isBusy = true;
 
         animator.SetTrigger("Dash");
         PlayAudio(araAudioSO.Move);
 
-        moveTargetPosition = GetMoveTargetPosition();
-        Helper.MoveToPosition(transform, moveTargetPosition, actionDuration);
+        _moveTargetPosition = GetMoveTargetPosition();
+        Helper.MoveToPosition(transform, _moveTargetPosition, actionDuration);
         yield return Helper.GetWaitForSeconds(actionDuration);
 
-        isBusy = false;
+        _isBusy = false;
     }
 
     private IEnumerator HandleInteract(Interactable interactable)
     {
-        isBusy = true;
+        _isBusy = true;
 
         switch (interactable)
         {
             case Pushable:
                 animator.SetTrigger("Attack");
-                interactable.Interact(this, playerDir);
+                interactable.Interact(this, _playerDir);
                 yield return Helper.GetWaitForSeconds(attackDuration);
-                isBusy = false;
+                _isBusy = false;
                 yield break;
             case Damageable:
                 animator.SetTrigger("Attack");
-                interactable.Interact(this, playerDir);
+                interactable.Interact(this, _playerDir);
                 yield return Helper.GetWaitForSeconds(attackDuration);
-                isBusy = false;
+                _isBusy = false;
                 yield break;
             case PillarLight:
                 animator.SetTrigger("Attack");
-                interactable.Interact(this, playerDir);
+                interactable.Interact(this, _playerDir);
                 yield return Helper.GetWaitForSeconds(attackDuration);
-                isBusy = false;
+                _isBusy = false;
                 yield break;
             case Pickupable:
                 interactable.Interact(this);
                 yield return StartCoroutine(HandleMovement());
-                isBusy = false;
+                _isBusy = false;
                 yield break;
             default:
                 break;
         }
 
-        interactable.Interact(this, playerDir);
+        interactable.Interact(this, _playerDir);
         yield return Helper.GetWaitForSeconds(actionDuration);
 
-        isBusy = false;
+        _isBusy = false;
     }  
 
     private void GameInput_OnSenterPerformed()
     {
-        if (!isSenterUnlocked)
+        if (!_isSenterUnlocked)
             return;
         StartCoroutine(ToggleSenter());
     }
 
     private void GameInput_OnHealthPotionPerformed()
     {
-        if (!isHealthPotionUnlocked)
+        if (!_isHealthPotionUnlocked)
             return;
-        if (health.IsHealthFull())
+        if (_health.IsHealthFull())
             return;
-        if (healthPotion.CurrentPotionAmount <= 0) 
+        if (_healthPotion.CurrentPotionAmount <= 0) 
             return;
-        if (healthPotion.IsHealthPotionOnCooldown)
+        if (_healthPotion.IsHealthPotionOnCooldown)
             return;
 
         PlayAudio(araAudioSO.Potion);
-        healthPotion.UsePotion();
+        _healthPotion.UsePotion();
     }
 
     private IEnumerator ToggleSenter()
@@ -209,50 +208,50 @@ public class Player : MonoBehaviour, IBroadcaster
             senterGameObject.transform.localPosition = new Vector3(100, 100, 0);
             yield return Helper.GetWaitForSeconds(0.05f);
             senterGameObject.SetActive(false);
-            isSenterEnabled = false;
+            _isSenterEnabled = false;
         }
         else
         {
             senterGameObject.transform.localPosition = new Vector3(0, 0.5f, 0);
             yield return Helper.GetWaitForSeconds(0.05f);
             senterGameObject.SetActive(true);
-            isSenterEnabled = true;
+            _isSenterEnabled = true;
         }
 
         PlayAudio(araAudioSO.Lantern);
-        OnSenterToggle?.Invoke(isSenterEnabled);
+        OnSenterToggle?.Invoke(_isSenterEnabled);
     }
 
     private IEnumerator ApplyDamageToPlayerCoroutine(bool knockBackPlayer, Vector2 knockbackTargetPosition)
     {
-        isTakeDamageOnCooldown = true;
+        _isTakeDamageOnCooldown = true;
 
-        animator.SetTrigger("OnHit");
+        _health.TakeDamage();
 
         //yield return StartCoroutine(Context.CameraShakeController.PlayCameraShake());
 
-        health.TakeDamage();
-
+        _hitFlashEffectMMFPlayer.PlayFeedbacks();
+        animator.SetTrigger("OnHit");
         damagedAnimator.Play("Ara_Damaged");
         PlayAudio(araAudioSO.MoveBox[UnityEngine.Random.Range(0, 3)]);
 
         if (knockBackPlayer)
+        {
             StartCoroutine(HandleKnockBack(knockbackTargetPosition));
+        }
 
-        OnInvulnerableVisualStart?.Invoke();
         yield return Helper.GetWaitForSeconds(takeDamageCooldown);
-        OnInvulnerableVisualEnd?.Invoke();
 
-        isTakeDamageOnCooldown = false;
+        _isTakeDamageOnCooldown = false;
     }
 
     private IEnumerator HandleKnockBack(Vector2 targetPosition)
     {
-        isKnocked = true;
-        moveTargetPosition = targetPosition;
+        _isKnocked = true;
+        _moveTargetPosition = targetPosition;
         Helper.MoveToPosition(transform, targetPosition, actionDuration);
         yield return Helper.GetWaitForSeconds(actionDuration);
-        isKnocked = false;
+        _isKnocked = false;
     }
 
     private void PlayAudio(AudioClip abilitySFX)
@@ -267,13 +266,13 @@ public class Player : MonoBehaviour, IBroadcaster
     public void ActivatePlayer()
     {
         gameObject.SetActive(true);
-        isBusy = false;
-        isKnocked = false;
+        _isBusy = false;
+        _isKnocked = false;
     }
 
     private Vector2 GetMoveTargetPosition()
     {
-        var moveTargetPosition = (Vector2)transform.position + playerDir;
+        var moveTargetPosition = (Vector2)transform.position + _playerDir;
         moveTargetPosition.x = Mathf.RoundToInt(moveTargetPosition.x);
         moveTargetPosition.y = Mathf.RoundToInt(moveTargetPosition.y);
         return moveTargetPosition;
