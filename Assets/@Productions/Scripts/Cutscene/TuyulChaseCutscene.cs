ab6f795bf4111aa90d72f5678e575f3ed15bb7ee
michsanl <desk.ichsan101@gmail.com>
2024-02-01 01:21:38 +0700

    Change level 5 restart handler show/hide logic

--- Assets/@Productions/Scripts/Cutscene/TuyulChaseCutscene.cs
+++ Assets/@Productions/Scripts/Cutscene/TuyulChaseCutscene.cs
@@ -11,29 +11,17 @@ public class TuyulChaseCutscene : MonoBehaviour
 {
     [SerializeField] private TuyulChaseTalkable _yulaTalkable;
     [SerializeField] private TuyulChaseTalkable _yuliTalkable;
-    [SerializeField] private GameObject _dialogueTrigger;
     [SerializeField] private GameObject _tuyulChaseLevelReset;
     [SerializeField] private GameObject _nextLevelGate;
 
-    private GameStateService _gameStateService;
     private Player _player;
 
     private void Awake()
     {
-        _gameStateService = SceneServiceProvider.GetService<GameStateService>();
         _player = SceneServiceProvider.GetService<PlayerManager>().Player;
 
         _yulaTalkable.OnAllTuyulHasBeenCaught += TuyulTalkable_OnAllTuyulCaught;
         _yuliTalkable.OnAllTuyulHasBeenCaught += TuyulTalkable_OnAllTuyulCaught;
-    }
-
-    public void OnConversationEnd()
-    {
-        if (_gameStateService.CurrentState == GameState.MainMenu) return;
-        
-        _player.UsePan = false;
-        _dialogueTrigger.SetActive(false);
-        SaveSystem.SaveToSlot(1);
     }
 
     private void TuyulTalkable_OnAllTuyulCaught(GameObject sender)
@@ -41,16 +29,15 @@ public class TuyulChaseCutscene : MonoBehaviour
         CompleteLevel(sender);
     }
 
-    private void CompleteLevel(GameObject sender)
+    private void CompleteLevel(GameObject lastTuyul)
     {
         // disable last active tuyul
         // give ara pan
         // save
         // disable level reset
 
-        sender.SetActive(false);
+        lastTuyul.SetActive(false);
         _nextLevelGate.SetActive(true);
-
         _player.UsePan = true;
 
         SaveSystem.SaveToSlot(1);
