6c8e87fb3cca0e517fd54dd64f5eeed13c2bf8c4
michsanl <desk.ichsan101@gmail.com>
2024-03-02 04:33:56 +0700

    Change level 6 restart logic

--- Assets/@Productions/Scripts/Cutscene/RaineLevelEvent.cs
+++ Assets/@Productions/Scripts/Cutscene/RaineLevelEvent.cs
@@ -8,16 +8,23 @@ using UnityEngine.Rendering.Universal;
 public class RaineLevelEvent : MonoBehaviour
 {
     [SerializeField] private Light2D _globalLight;
+    [SerializeField] private GameObject _restartHandler;
 
     private void Start()
     {
         if (DialogueLua.GetVariable("Level_6_Puzzle_Done").asBool) 
         {
             _globalLight.intensity = 1f;
+            _restartHandler.SetActive(true);
         }
         else
         {
             _globalLight.intensity = 0f;
+        }
+
+        if (DialogueLua.GetVariable("Level_7_Done").asBool)
+        {
+            _restartHandler.SetActive(false);
         }
     }
 
@@ -26,15 +33,22 @@ public class RaineLevelEvent : MonoBehaviour
         if (DialogueLua.GetVariable("Level_6_Puzzle_Done").asBool) 
         {
             _globalLight.intensity = 1f;
+            _restartHandler.SetActive(true);
         }
         else
         {
             _globalLight.intensity = 0f;
+        }
+
+        if (DialogueLua.GetVariable("Level_7_Done").asBool)
+        {
+            _restartHandler.SetActive(false);
         }
     }
 
     private void OnDisable()
     {
         _globalLight.intensity = 1f;
+        _restartHandler.SetActive(false);
     }
 }
